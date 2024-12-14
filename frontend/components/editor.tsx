1df7c92c2ea832e4afec810dec09ba433e290b70
Hitoshi-Noborikawa <dengchuanrenzhi@gmail.com>
2024-12-14 19:51:18 +0900

    記事をgetするとこまで

--- frontend/components/editor.tsx
+++ frontend/components/editor.tsx
@@ -7,13 +7,26 @@ import { useState } from "react";
 
 const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
 
-const Editor = () => {
-  const [value, setValue] = useState<string>("**Hello world!!!**");
+interface EditorProps {
+  userId: string | undefined;
+  article: string;
+}
+
+const Editor: React.FC<EditorProps> = ({ userId, article }) => {
+  const [value, setValue] = useState<string>(article);
 
   return (
-    <div style={{ padding: "20px" }}>
+    <>
+      <div style={{ padding: "20px" }}>
         <MDEditor value={value} onChange={setValue} height={400} width={400} />
-    </div>
+      </div>
+      <div>
+        Logged in user ID: {userId || "Guest"}
+      </div>
+      <div>
+        {article}
+      </div>
+    </>
   );
 };
 
