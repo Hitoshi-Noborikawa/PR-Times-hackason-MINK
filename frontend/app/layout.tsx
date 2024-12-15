773683d4ed7e801738a89de9bae9178e2c671fb2
Hitoshi-Noborikawa <dengchuanrenzhi@gmail.com>
2024-12-15 11:53:32 +0900

    ヘッダーのレイアウトを修正

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
+import Link from "next/link";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
@@ -33,9 +34,9 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
+                  <Link className="flex gap-5 items-center font-semibold" href="/">
                     MINK APP
-                  </div>
+                  </Link>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
               </nav>
--- frontend/components/header-auth.tsx
+++ frontend/components/header-auth.tsx
@@ -1,5 +1,4 @@
 import { signOutAction } from "@/app/actions";
-import Link from "next/link";
 import { Button } from "./ui/button";
 import { createClient } from "@/utils/supabase/server";
 
@@ -10,23 +9,14 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  return user ? (
+  return user && (
     <div className="flex items-center gap-4">
       {user.email}
       <form action={signOutAction}>
         <Button type="submit" variant={"outline"}>
-          Sign out
+          ログアウト
         </Button>
       </form>
-    </div>
-  ) : (
-    <div className="flex gap-2">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
-      </Button>
     </div>
   );
 }
