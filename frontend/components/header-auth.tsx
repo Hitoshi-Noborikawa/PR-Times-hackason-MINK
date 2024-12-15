f3187fe9a78d9651206e335172da9281e7ad732d
Hitoshi-Noborikawa <dengchuanrenzhi@gmail.com>
2024-12-15 14:29:16 +0900

    プレスリリースの設定画面リンクを追加

--- frontend/components/header-auth.tsx
+++ frontend/components/header-auth.tsx
@@ -1,5 +1,6 @@
 import { signOutAction } from "@/app/actions";
 import { Button } from "./ui/button";
+import Link from "next/link";
 import { createClient } from "@/utils/supabase/server";
 
 export default async function AuthButton() {
@@ -11,6 +12,13 @@ export default async function AuthButton() {
 
   return user && (
     <div className="flex items-center gap-4">
+      <div>
+      <Link
+        href="/job"
+      >
+        プレスリリース自動生成の設定
+        </Link>
+      </div>
       {user.email}
       <form action={signOutAction}>
         <Button type="submit" variant={"outline"}>
